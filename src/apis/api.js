f4926023a7cd8f5ff5cf9eb8b5145038277afdc6
AA0027 <whckdtjd456@naver.com>
2024-08-15 18:17:05 +0900

    axios cors전역설정

--- src/apis/api.js
+++ src/apis/api.js
@@ -5,7 +5,8 @@ import axios from 'axios'
 //  axios.create({사용자 지정 config})
 
 const api = axios.create();
-
+axios.defaults.withCredentials = true;
+// 출처: https://artistjay.tistory.com/26 [Jlog:티스토리]
 export const SERVER_HOST = `/api`;
 export const HOST = `/api/channel`;
 // export const SERVER_HOST = `http://3.35.4.138:8080/api`;
